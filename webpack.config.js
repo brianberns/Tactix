716433ec4502b487e289d089fa64231fb1a4a20b
Brian Berns <brianberns@gmail.com>
2024-02-12 10:13:47 -0500

    Add project files.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/App.fs.js",
+    output: {
+        path: path.join(__dirname, "./public"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        static: "./public",
+        port: 8080
+    },
+    module: {
+    }
+}
