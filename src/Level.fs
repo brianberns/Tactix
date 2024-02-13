2a6c3f6aa01b750eb16aebd445da847e882d32ea
Brian Berns <brianberns@gmail.com>
2024-02-13 10:58:05 -0500

    Modularized.

---
+++ src/Level.fs
@@ -0,0 +1,57 @@
+﻿namespace Tactix
+
+[<RequireQualifiedAccess>]
+type TacticType =
+    | Exact
+    | Intro
+    | Apply
+
+module TacticType =
+
+    let emoji = function
+        | TacticType.Exact -> "🎆"
+        | TacticType.Intro -> "🚀"
+        | TacticType.Apply -> "👣"
+
+type Level =
+    {
+        Goal : Type
+        Terms : Set<Term>
+        TacticTypes : Set<TacticType>
+        Instructions : string
+    }
+
+module Level =
+
+    module private Term =
+
+        let create typ =
+            Term.create $"H{typ}" typ
+
+    let private level0 =
+        {
+            Goal = P
+            Terms =
+                set [
+                    Term.create P
+                    Term.create Q
+                ]
+            TacticTypes =
+                set [
+                    TacticType.Exact
+                ]
+            Instructions =
+                let exact = TacticType.emoji TacticType.Exact
+                $"Drag {exact} onto the symbol that matches the goal."
+        }
+
+    let levels =
+        [|
+            level0
+        |]
+
+    let initializeProof level =
+        {
+            Goal = Some level.Goal
+            Terms = level.Terms
+        }
--- src/Model.fs
+++ src/Model.fs
@@ -2,59 +2,6 @@
 
 open Elmish
 
-[<RequireQualifiedAccess>]
-type TacticType =
-    | Exact
-    | Intro
-    | Apply
-
-module TacticType =
-
-    let emoji = function
-        | TacticType.Exact -> "🎆"
-        | TacticType.Intro -> "🚀"
-        | TacticType.Apply -> "👣"
-
-type Level =
-    {
-        Goal : Type
-        Terms : Set<Term>
-        TacticTypes : Set<TacticType>
-        Instructions : string
-    }
-
-module Level =
-
-    module private Term =
-
-        let create typ =
-            Term.create $"H{typ}" typ
-
-    let levels =
-        [|
-            {
-                Goal = P
-                Terms =
-                    set [
-                        Term.create P
-                        Term.create Q
-                    ]
-                TacticTypes =
-                    set [
-                        TacticType.Exact
-                    ]
-                Instructions =
-                    let exact = TacticType.emoji TacticType.Exact
-                    $"Drag {exact} onto the symbol that matches the goal."
-            }
-        |]
-
-    let initializeProof level =
-        {
-            Goal = Some level.Goal
-            Terms = level.Terms
-        }
-
 type Model =
     {
         LevelIndex : int
