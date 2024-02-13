def90a55081f4c276ec3cd02d885a631d35e3dc1
Brian Berns <brianberns@gmail.com>
2024-02-13 09:26:18 -0500

    Modularized.

---
+++ src/Audio.fs
@@ -0,0 +1,21 @@
+﻿namespace Tactix
+
+open Browser.Types
+open Fable.Core
+
+// https://thesharperdev.com/snippets/fsharp-fable-play-audio/
+module Audio =
+
+    let [<Global("Audio")>] private factory : HTMLAudioElementType = jsNative
+
+    let private play src =
+        let audio = factory.Create()
+        audio.src <- src
+        audio.volume <- 0.5
+        audio.play()
+
+    let playReward () =
+        play "https://neal.fun/infinite-craft/reward.mp3"
+
+    let playError () =
+        play "https://neal.fun/infinite-craft/error.mp3"
