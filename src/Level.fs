2e68dd21be44cd46e215160558860737e51f8f58
Brian Berns <brianberns@gmail.com>
2024-02-19 23:00:18 -0500

    Added a level.

--- src/Level.fs
+++ src/Level.fs
@@ -322,6 +322,29 @@ module Level =
             Instructions = ""
         }
 
+    let private level19 =
+        {
+            Goal = Type.not (Product [p; q])
+            Terms =
+                terms [
+                    Sum [
+                        Type.not p
+                        Type.not q
+                    ]
+                ]
+            TacticTypes =
+                set [
+                    TacticType.Exact
+                    TacticType.Intro
+                    TacticType.Apply
+                    TacticType.Cases
+                    TacticType.Left
+                    TacticType.Right
+                    TacticType.Split
+                ]
+            Instructions = ""
+        }
+
     let levels =
         [|
             level1
@@ -342,6 +365,7 @@ module Level =
             level16
             level17
             level18
+            level19
         |]
 
     let initializeProof level =
