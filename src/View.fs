3d86bb1c68ef0a963cfc0f1082fc9322fcb2b0c7
Brian Berns <brianberns@gmail.com>
2024-04-12 13:55:33 -0400

    Modularized.

--- src/View.fs
+++ src/View.fs
@@ -1,40 +1,6 @@
 ﻿namespace Tactix
 
-open Browser.Types
 open Feliz
-
-/// Data carried during an HTML drag/drop operation.
-type private DragData =
-    {
-        /// Tactic type being dragged.
-        TacticType : TacticType
-    }
-
-module private DragData =
-
-    // https://stackoverflow.com/questions/40940288/drag-datatransfer-data-unavailable-in-ondragover-event
-    // https://stackoverflow.com/questions/31915653/how-to-get-data-from-datatransfer-getdata-in-event-dragover-or-dragenter
-    let mutable shared : Option<DragData> = None
-
-    /// Sets drag data for the given event.
-    let private setData dragData (evt : DragEvent) =
-        // evt.dataTransfer.setData
-        shared <- Some dragData
-
-    /// Gets drag data for the given event.
-    let private getData (evt : DragEvent) =
-        // evt.dataTransfer.getData
-        shared
-
-    /// Sets the tactic type being dragged.
-    let setTacticType tacticType evt =
-        setData { TacticType = tacticType } evt
-
-    /// Gets the tactic type being dragged.
-    let getTacticType evt =
-        match getData evt with
-            | Some dragData -> dragData.TacticType
-            | None -> failwith "Unexpected"
 
 module View =
 
