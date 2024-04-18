ac9ed3bc86b68eefa7dfb106effb07eb51204e84
Brian Berns <brianberns@gmail.com>
2024-04-18 00:59:37 -0400

    Warn for small screen size.

--- src/App.fs
+++ src/App.fs
@@ -1,9 +1,15 @@
 namespace Tactix
 
+open Browser
 open Elmish
 open Elmish.React
 
 module App =
+
+    // window.alert($"Your screen is {window.innerWidth}x{window.innerHeight} pixels")
+    if window.innerWidth < 600
+        || window.innerHeight < 900 then
+        window.alert("This game works best on a larger screen, but we'll give it a try!")
 
     Program.mkProgram Model.init Model.update View.render
         |> Program.withReactSynchronous "elmish-app"
